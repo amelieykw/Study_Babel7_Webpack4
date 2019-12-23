fcf3721c1d1fedfa5b20a7347d85168ac69d82e7
amelieykw <yu.kaiwen.amelie@gmail.com>
2019-12-24 05:19:16 +0800

    create restful apis on back-end and create react on front-end

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,5 +1,73 @@
 import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+import uuidv4 from 'uuid/v4';
 
-console.log('Hello Node.js project.');
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-console.log(process.env.MY_SECRET);
+app.listen(process.env.PORT, () =>
+    console.log(`Example app listening on port ${process.env.PORT}!`),
+);
+
+let users = {
+    1: {
+        id: '1',
+        username: 'Robin Wieruch',
+    },
+    2: {
+        id: '2',
+        username: 'Dave Davids',
+    },
+};
+
+app.get('/users', (req, res) => {
+    return res.send(Object.values(users));
+});
+app.get('/users/:userId', (req, res) => {
+    return res.send(users[req.params.userId]);
+});
+
+let messages = {
+    1: {
+        id: '1',
+        text: 'Hello World',
+        userId: '1',
+    },
+    2: {
+        id: '2',
+        text: 'By World',
+        userId: '2',
+    },
+};
+
+app.get('/messages', (req, res) => {
+    return res.send(Object.values(messages));
+});
+app.get('/messages/:messageId', (req, res) => {
+    return res.send(messages[req.params.messageId]);
+});
+app.post('/messages', (req, res) => {
+    const id = uuidv4();
+    const message = {
+        id,
+        text: req.body.text,
+    };
+    messages[id] = message;
+    return res.send(message);
+});
+
+app.get('/', (req, res) => {
+    return res.send('Received a GET HTTP method');
+});
+app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+});
+app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+});
+app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+});
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,17 +1,15 @@
-import HelloWorldButton from "./components/hello-world-button/hello-world-button.js";
-import Heading from './components/heading/heading.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const heading = new Heading();
-heading.render();
 
-const helloWorldButton = new HelloWorldButton();
-helloWorldButton.render();
+import App from './App';
 
-if (process.env.NODE_ENV === 'production') {
-    console.log('Production mode');
-} else if (process.env.NODE_ENV === 'development') {
-    console.log('Development mode');
-}
+const title = 'React with Webpack and Babel';
+
+ReactDOM.render(
+    <App title={title} />,
+    document.getElementById('app')
+);
 
 if (module.hot) {
     module.hot.accept();
